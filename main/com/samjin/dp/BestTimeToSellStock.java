966d8cb8ea86e512c8d69f3d8e046059da7243dc
Su(Sam) Jin <sjjin@amazon.com>
2020-05-17 01:51:25 -0700

    add snapshot array

--- main/com/samjin/dp/BestTimeToSellStock.java
+++ main/com/samjin/dp/BestTimeToSellStock.java
@@ -7,15 +7,15 @@ public class BestTimeToSellStock {
      */
     public int maxProfit(int[] prices) {
 
-        if(prices == null ||prices.length < 2) return 0;
+        if (prices == null || prices.length < 2) return 0;
 
         int minPrice = prices[0];
 
         int profit = 0;
 
-        for(int i = 1; i < prices.length; i++){
+        for (int i = 1; i < prices.length; i++) {
             profit = Math.max(prices[i] - minPrice, profit);
-            if(prices[i] < minPrice) minPrice=prices[i];
+            if (prices[i] < minPrice) minPrice = prices[i];
         }
 
         return profit;
@@ -27,11 +27,53 @@ public class BestTimeToSellStock {
      */
     public int maxProfit2(int[] prices) {
         int profit = 0;
-        for( int i = 1; i < prices.length; i++ ){
-            if( prices[i] > prices[i-1] ){
-                profit += prices[i] - prices[i-1];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
             }
         }
         return profit;
     }
+
+
+    /**
+     * Can have most two transaction
+     */
+
+    public int maxProfit3(int[] prices) {
+        return 0;
+    }
+
+
+    /**
+     * Can have most K transaction
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit4(int[] prices) {
+        return 0;
+    }
+
+    /**
+     * Can have as many as transactions you want with fee.
+     */
+    public int maxProfit5(int[] prices) {
+        return 0;
+
+    }
+
+    /**
+     * Can have as many as transactions as with cool down
+     * @param prices
+     * @return
+     */
+    public int maxProfit6(int[] prices) {
+        return 0;
+
+    }
+
 }
+
+
+
